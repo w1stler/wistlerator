69cf458d613969c9b2a96737b51a527de18985be
w1stler <wojciech.patelka@gmail.com>
2019-02-14 22:37:40 +0100

    Initial schema + migration

--- wistlerator/samples/models.py
+++ wistlerator/samples/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Sample(models.Model):
+    serial_number = models.CharField(max_length=25)
+    name = models.CharField(max_length=50)
+    arrival_date = models.DateTimeField('arrival date')
+    content = models.CharField(max_length=1000)
+    comments = models.CharField(max_length=1000)
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.serial_number
+
+
+class MediaCompany(models.Model):
+    name = models.CharField(max_length=50)
+    street = models.CharField(max_length=25)
+    postal_code = models.CharField(max_length=6)
+    city = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
